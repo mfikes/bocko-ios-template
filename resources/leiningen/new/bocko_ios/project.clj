3d2b6de1cb3006e9184e03c25d367b487b69396a
Mike Fikes <mike@fikesfarm.com>
2015-05-24 22:54:19 -0400

    Clean up project file

--- resources/leiningen/new/bocko_ios/project.clj
+++ resources/leiningen/new/bocko_ios/project.clj
@@ -1,11 +1,10 @@
-(defproject bocko-ios "0.2.0"
+(defproject bocko-ios "0.1.0"
   :description "Use Bocko on iOS"
-  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
                  [org.clojure/clojurescript "0.0-3269"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [bocko "0.3.0"]
                  [org.omcljs/ambly "0.3.0"]]
-  :plugins [[lein-cljsbuild "1.0.5"]]
+  :plugins [[lein-cljsbuild "1.0.6"]]
   :source-paths ["src"]
   :clean-targets ["target" "out"]
   :cljsbuild {:builds {:dev
