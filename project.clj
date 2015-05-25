c8ed98f8bf168151a33b4047a0fe8c2611166568
Mike Fikes <mike@fikesfarm.com>
2015-05-24 22:41:16 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject bocko-ios/lein-template "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Template for Bocko iOS"
+  :url "https://github.com/mfikes/bocko-ios-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
