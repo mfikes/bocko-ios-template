dd099cb555895c953d4033fb45d831d33eef65f3
Mike Fikes <mike@fikesfarm.com>
2017-06-03 09:48:06 -0400

    1.1.0
    Update deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bocko-ios/lein-template "1.0.0"
+(defproject bocko-ios/lein-template "1.1.0"
   :description "Template for Bocko iOS"
   :url "https://github.com/mfikes/bocko-ios-template"
   :license {:name "Eclipse Public License"
